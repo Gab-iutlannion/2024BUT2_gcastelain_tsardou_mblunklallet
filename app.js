22cd629cbb823cd1e7cc7db652f5d1f87e04936b
Thibault Sardou <96932771+ThIbauLTSardou@users.noreply.github.com>
2024-10-10 12:06:29 +0200

    connexion BDD

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
 const express = require('express');
-const app = express ();
+const app = express();
+
+const utilisateurs = require("./models/utilisateurs.js")
 
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'));
 
-app.get('/', function(req, res){
-    res.render('index');
+
+app.get('/', async(req, res) => { 
+    try {
+        let user = await utilisateurs.getUserById(1);
+        res.render('index', { user });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send('Erreur lors de la récuperation des données');
+    };
 });
 
-app.use(function (req, res){
-    res.status(404).render('404');
 
-});
+app.use((req, res) => {
+    res.status(404).render("404");
+})
 
-app.listen(3000, function(){
-    console.log('Server running on port 3000');
-});
+app.listen(3000 , () => {
+   console.log('Server running on port 3000');
+}
+)
