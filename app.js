95632afa4e3d240aa33eb2490906642fc558693d
Thibault Sardou <96932771+ThIbauLTSardou@users.noreply.github.com>
2024-10-15 08:59:20 +0200

    connexion

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 const utilisateurs = require("./models/utilisateurs.js")
+const database = require('./models/database.js');
+const md5 = require('md5');
+const session = require('express-session');
 
 app.set('view engine', 'ejs');
 
@@ -28,6 +32,77 @@ app.get('/acceuil', function (req, res) {
 app.get('/connexion', function (req, res) {
     res.render("./connexion");
 });
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(session({
+    secret: 'patate',                   // Clé secrète utilisée pour signer le cookie de session
+    resave: false,                     // Ne pas enregistrer la session si elle n'a pas été modifiée
+    saveUninitialized: true,           // Sauvegarder une session non initialisée
+    cookie: { secure: false }          // Assurez-vous que "secure" est à true en production (HTTPS)
+}));
+
+app.post('/connexion', (req, res) => {
+    const identifiant = req.body.identifiant;  
+    const mdp = req.body.mdp;
+
+    if (identifiant && mdp){
+        const user = "SELECT login, password FROM utilisateur WHERE login = ?";
+        const role = "SELECT type_utilisateur FROM utilisateur WHERE login = ? ";
+
+        
+            database.query(user, [identifiant], (err, userResult) =>{
+            if (err){
+                res.send('L identifiant n est pas bon');
+                console.error('L idenfifiant n est pas bon', err);
+            }
+            if (userResult.length > 0){
+
+                if(userResult[0].password === md5(mdp)){
+                    console.log(userResult);
+    
+                    database.query(role, identifiant, (err, roleResult) =>{
+                        console.log(roleResult);
+                        if (err){
+                            res.send('Aucun rôle ne vous est attribué.');
+                            console.error('Erreur lors de la requête SQL pour le rôle:', err);
+                        }
+                        if (roleResult.length > 0){
+                            req.session.login = true;
+                            req.session.username = identifiant;
+                            req.session.role = roleResult[0].type_utilisateur;
+        
+                                console.log('l utilisateur est connécté en tant que ' + roleResult[0].type_utilisateur );
+                        }
+                        else{
+                            res.send('Aucun rôle n est attribué.');
+                        }   
+                       
+                    });
+                }
+                else{
+                    res.send('Mot de passe incorrect.');
+                }
+            }  
+            
+            else{
+                res.send('Identifiant ou mot de passe éroné');
+                console.error('l identifiant ou le mot de passe n est pas bon', err);
+            }
+            
+        
+    });
+        
+    }
+    else{
+        res.send('Veuillez remplir tout les champs');
+        console.error('Tout les champs n ont pas été remplis', err);
+    }
+   
+});
+
 
 
 
--- models/utilisateurs.js
+++ models/utilisateurs.js
@@ -13,19 +13,9 @@ async function getUserById (id) {
 };
 
 
-async function Connexion (login, password) {
-    user ="SELECT login, password FROM utilisateur WHERE login LIKE ? ";
-    return new Promise((resolve, reject) => {
-        database.query(sql, login, password,  (err, results) => {
-            if (err) {
-                return reject(err);
-            }
-            resolve(results);
-        });
-    });
-};
+
 
 
 module.exports = {getUserById};
-module.exports = {Connexion};
+
 
